e87336c69236f4700425d780799861703e29fb92
Audrey Kim <afk002@ucsd.edu>
2026-06-03 11:24:04 -0700

    Added function 6

--- flufinder.R
+++ flufinder.R
@@ -4,3 +4,16 @@ split_peptides <- function(peptides) {
   library(stringr)
   lapply(peptides, str_split, pattern="")
 }
+
+# Function 6
+
+ggbarplot <- function(peptide_counts_table) {
+  
+  library(ggplot2)
+  ggplot(peptide_counts_table) +
+    aes(rownames(peptide_counts_table), peptide_counts) +
+    geom_col(fill="blue", width=0.5) +
+    theme_bw() +
+    labs(x="Flu Strain", y="Peptide Counts")
+  
+}
